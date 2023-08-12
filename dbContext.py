d9e45b281f16431e01fe6a81c83a61a62d0d0d49
Samir Khadka <samir.khadka@clinis.com.au>
2023-08-12 20:26:11 +0545

    deleted model , dbcontext commented all is done from app.py

--- dbContext.py
+++ dbContext.py
@@ -1,11 +1,11 @@
-from app import app, db
-from models import User, Brand, DiscountCard, VehicleType, Location, CheckIn, CheckOut, Move
+# from app import app, db
+# from models import User, Brand, DiscountCard, VehicleType, Location, CheckIn, CheckOut, Move
 
-# Ensure proper app and db initialization here
+# # Ensure proper app and db initialization here
 
-with app.app_context():
-    try:
-        db.create_all()
-        print("Database tables created successfully.")
-    except Exception as e:
-        print("An error occurred while creating database tables:", e)
+# with app.app_context():
+#     try:
+#         db.create_all()
+#         print("Database tables created successfully.")
+#     except Exception as e:
+#         print("An error occurred while creating database tables:", e)
